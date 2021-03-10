351ed955f58080437e2794f95dc6072e8cf2a1a6
Abdelkrimnaji <naji.abdelkrim25@gmail.com>
2021-03-10 12:02:37 +0100

    Refactoring TutoCircles file

--- CircleProject/Models/CircleObject.swift
+++ CircleProject/Models/CircleObject.swift
@@ -11,28 +11,29 @@ import SwiftUI
 struct CircleObject {
     let name: String
     var users: [User] = []
-    let image: Image?
+    let image: Image
     let color: Color
     let type: CircleType
+    let size: Double
 }
 
 extension CircleObject{
     static var circles: [CircleObject]{
         return [
-            CircleObject(name: "Cercle Familial", users: [], image: Image("CercleFamille"), color: Color(red: 253/255, green: 143/255, blue: 147/255), type: .familial),
-            CircleObject(name: "Cercle Amical", users: [User(name: "Abdel", email: "abdelkrim@gmail.com", password: "pass", userImage: "", age: "23", gender: "Homme", location: "Gray", categories: ["Sport"])], image: Image("CercleAmical"), color: Color(red: 214/255, green: 101/255, blue: 105/255), type: .amical),
-            CircleObject(name: "Cercle Professionnel", users: [], image: Image("CercleProfessionnel"), color: Color(red: 235/255, green: 71/255, blue: 78/255), type: .professioonnel),
-            CircleObject(name: "Cercle Public", users: [], image: Image("CerclePublic"), color: Color(red: 168/255, green: 42/255, blue: 48/255), type: .public)]
+            CircleObject(name: "Cercle Familial", users: [], image: Image("CercleFamille"), color: Color(red: 253/255, green: 143/255, blue: 147/255), type: .familial, size: 0.25),
+            CircleObject(name: "Cercle Amical", users: [User(name: "Abdel", email: "abdelkrim@gmail.com", password: "pass", userImage: "", age: "23", gender: "Homme", location: "Gray", categories: ["Sport"])], image: Image("CercleAmical"), color: Color(red: 214/255, green: 101/255, blue: 105/255), type: .amical, size: 0.45),
+            CircleObject(name: "Cercle Professionnel", users: [], image: Image("CercleProfessionnel"), color: Color(red: 235/255, green: 71/255, blue: 78/255), type: .professioonnel, size: 0.65),
+            CircleObject(name: "Cercle Public", users: [], image: Image("CerclePublic"), color: Color(red: 168/255, green: 42/255, blue: 48/255), type: .public, size: 0.8)]
     }
     static var familial: CircleObject{
-        CircleObject(name: "Cercle Familial", users: [], image: Image("CercleFamille"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .familial)
+        CircleObject(name: "Cercle Familial", users: [], image: Image("CercleFamille"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .familial, size: 0.25)
     }
-    static var amical: CircleObject{
-        CircleObject(name: "Cercle Amical", users: [], image: Image("CercleAmical"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .amical)
-    }
-    static var professionnel: CircleObject{
-        CircleObject(name: "Cercle Professionnel", users: [], image: Image("CercleProfessionnel"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .professioonnel)
-    }
+//    static var amical: CircleObject{
+//        CircleObject(name: "Cercle Amical", users: [], image: Image("CercleAmical"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .amical)
+//    }
+//    static var professionnel: CircleObject{
+//        CircleObject(name: "Cercle Professionnel", users: [], image: Image("CercleProfessionnel"), color: Color(red: 0.996, green: 0.557, blue: 0.576), type: .professioonnel)
+//    }
     
     func addUser(_ user: User) {
         print("Ajout d'utilisateur")
--- CircleProject/Views/Messages/MessagesView.swift
+++ CircleProject/Views/Messages/MessagesView.swift
@@ -8,13 +8,47 @@
 import SwiftUI
 
 struct MessagesView: View {
+    @State var noticationsList: [Notification] = []
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            ForEach(noticationsList, id: \.id){ notification in
+                NotificationsItem(notification: notification)
+            }
+        }.onAppear{
+            Api().getNotifications { (notification,error)  in
+                if error != nil{
+                    print(error!.localizedDescription)
+                }else if let notications = notification{
+                    self.noticationsList = notications
+                }
+            }
+        }
     }
 }
 
 struct MessagesView_Previews: PreviewProvider {
     static var previews: some View {
         MessagesView()
+    }
+}
+
+struct NotificationsItem: View  {
+    var notification: Notification
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: "person.circle.fill")
+                VStack(alignment: .leading){
+                    Text(notification.text)
+                    Text(" 5 mars, à 20h30")
+                        .foregroundColor(.gray)
+                        .font(.subheadline)
+                }
+                Spacer()
+            }.padding()
+            Divider()
+        }
+//        .background(Color.blue.opacity(0.3))
     }
 }
